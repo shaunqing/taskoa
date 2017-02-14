57a478e0009ba52fcaa66cd29cc9843b161f2934
shaunqing <shaunqing@gmail.com>
2017-02-14 10:12:34 +0800

    管理员控制器

---
+++ src/cn/taskoa/sys/web/AdminController.java
@@ -0,0 +1,27 @@
+package cn.taskoa.sys.web;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.taskoa.common.web.BaseController;
+import cn.taskoa.task.service.TaskService;
+
+@Controller
+@RequestMapping("/a")
+public class AdminController extends BaseController {
+
+	@Autowired
+	private TaskService taskService;
+
+	/**
+	 * 测试
+	 */
+	@RequestMapping("/loginout")
+	public String loginout(HttpSession session) {
+		taskService.find(0, 0);
+		return "redirect:sys";
+	}
+}
